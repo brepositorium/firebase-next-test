4528d433e0c9749891fd94feeefce7927fb00227
brepositorium <rcryptob@protonmail.ch>
2024-05-11 00:17:47 +0300

    add NextResponse

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -3,18 +3,19 @@ import { collection, query, where, getDocs } from 'firebase/firestore';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-    // Manually set CORS headers
-    const response = new NextResponse();
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-    response.headers.set('Content-Type', 'application/json');
-
     // Check for event name
     const eventName = request.nextUrl.searchParams.get('eventName');
     
     if (!eventName) {
-        return response;
+        return new NextResponse(JSON.stringify({ error: 'Event name is required' }), {
+            status: 400,
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }),
+        });
     }
 
     try {
@@ -23,12 +24,36 @@ export async function GET(request: NextRequest) {
         const querySnapshot = await getDocs(q);
         
         if (querySnapshot.empty) {
-            return response;
+            return new NextResponse(JSON.stringify({ error: 'No event found with the specified name' }), {
+                status: 404,
+                headers: new Headers({
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                }),
+            });
         }
 
         const events = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        return response;
+        return new NextResponse(JSON.stringify(events), {
+            status: 200,
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }),
+        });
     } catch (error) {
-        return response;
+        return new NextResponse(JSON.stringify({ error }), {
+            status: 500,
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, HEAD, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }),
+        });
     }
 }
